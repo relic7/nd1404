8fc0023173fbd712a293d5974d9c57adecfbbaf1
John Bragato <jbragato@yahoo.com>
2014-08-25 23:25:55 -0400

    db syncs

--- src/dam/celeryconfig.py
+++ src/dam/celeryconfig.py
@@ -1,6 +1,5 @@
 BROKER_URL = "amqp://guest:guest@localhost:5672//"
 CELERY_RESULT_BACKEND = "amqp"
-CELERY_ACCEPT_CONTENT = ['json', 'msgpack', 'yaml'] #'pickle'
 CELERY_IMPORTS = ("dam.mprocessor.processor", 
                   "dam.mprocessor.servers.generic_cmd",
                   "dam.mprocessor.servers.xmp_embedder",
