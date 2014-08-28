ae9f16da813cb22c47d05954d9810a7d5340a6de
johnb <john.bragato@gmail.com>
2014-08-28 17:20:11 -0400

    x≈Y

--- src/dam/manage.py
+++ src/dam/manage.py
@@ -34,7 +34,7 @@ Command.handle_noargs = handle_noargs
 # from django.core.management import execute_manager
 try:
 
-    import settings # Assumed to be in the same directory.
+    import dam.settings # Assumed to be in the same directory.
 except ImportError:
     import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
@@ -43,7 +43,7 @@ except ImportError:
 #if __name__ == "__main__":
 #    execute_manager(settings)
 if __name__ == "__main__":
-    import os, sys, settings
+    import os, sys
     sys.path.append(os.path.dirname(__file__))
     sys.path.append(os.path.dirname(os.path.dirname(__file__)))
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dam.settings")
